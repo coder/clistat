0abe23786df58c543f3b38c2c2666570c42a0d0e
Danielle Maywood <danielle@coder.com>
2025-03-27 15:42:58 +0000

    chore: check if tests fail when change is removed

--- stat_linux.go
+++ stat_linux.go
@@ -2,33 +2,33 @@ package clistat
 
 import (
 	"runtime"
-	"strings"
+	// "strings"
 
 	"github.com/spf13/afero"
 )
 
 func numCPU(fs afero.Fs) int {
-	procCPUInfo, err := afero.ReadFile(fs, "/proc/cpuinfo")
-	if err != nil {
-		// If we have an issue reading from `/proc/cpuinfo`
-		// we instead fallback to `runtime.NumCPU`.
-		return runtime.NumCPU()
-	}
+	// procCPUInfo, err := afero.ReadFile(fs, "/proc/cpuinfo")
+	// if err != nil {
+	// If we have an issue reading from `/proc/cpuinfo`
+	// we instead fallback to `runtime.NumCPU`.
+	return runtime.NumCPU()
+	// }
 
-	cpuInfo := strings.Split(string(procCPUInfo), "\n")
-	cpuCount := 0
+	// cpuInfo := strings.Split(string(procCPUInfo), "\n")
+	// cpuCount := 0
 
-	for _, line := range cpuInfo {
-		if line == "" {
-			continue
-		}
+	// for _, line := range cpuInfo {
+	// 	if line == "" {
+	// 		continue
+	// 	}
 
-		parts := strings.Split(line, ":")
+	// 	parts := strings.Split(line, ":")
 
-		if len(parts) > 1 && strings.TrimSpace(parts[0]) == "processor" {
-			cpuCount += 1
-		}
-	}
+	// 	if len(parts) > 1 && strings.TrimSpace(parts[0]) == "processor" {
+	// 		cpuCount += 1
+	// 	}
+	// }
 
-	return cpuCount
+	// return cpuCount
 }
